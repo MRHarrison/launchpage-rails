f0585b1991038c6dfefda8b9e9cba807a24bc0ef
Matthew Harrison <mrussellharrison@gmail.com>
2013-08-26 08:52:58 -0700

    action mailer adds

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,16 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+ActionMailer::Base.smtp_settings = {
+    :user_name      => 'mrharrison',
+    :password       => 'l0v3l1f3!',
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :domain => 'betathread.com',
+    :enable_starttls_auto => true
+}
+
+
 # Initialize the rails application
 LaunchpageRails::Application.initialize!
